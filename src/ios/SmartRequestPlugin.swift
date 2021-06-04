d3380ff9e44a860ff3884d80e57558ac5a2cfc5f
Janeeczek <mazurekjan1@gmail.com>
2021-06-04 09:37:43 +0200

    poprawnie zaploadowany IOS

--- src/ios/SmartRequestPlugin.swift
+++ src/ios/SmartRequestPlugin.swift
@@ -1,63 +1,75 @@
 
-import UserNotifications
+@objc(SmartRequestPlugin)
+class SmartRequestPlugin : CDVPlugin {
 
-@objc(GpsPlugin)
-class GpsPlugin : CDVPlugin {
-    let notificationCenter = UNUserNotificationCenter.current()
-    let options: UNAuthorizationOptions = [.alert, .sound, .badge]
-    
-    
-    
     override func pluginInitialize() {
         super.pluginInitialize()
         print("SmartRequestPlugin :: Inizjalizacja")
-        
-        //initializeLocationBest()
-        //initializeLocationWorse()
-        inistializeNotification()
-        sendNotification(title: "SmartRequestPlugin", body: "Inicjalizacja poprawna")
-    }
-    func inistializeNotification() {
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
-            if success {
-                print("SmartRequestPlugin :: Powiadomienia ustawione poprawnie!!")
-            } else if let error = error {
-                print(error.localizedDescription)
-            }
-        }
-        notificationCenter.getNotificationSettings { (settings) in
-          if settings.authorizationStatus != .authorized {
-            print("SmartRequestPlugin :: Powiadomienia zakazane przez użytkownika")
-          }
-        }
-        
-    }
-    func sendNotification(title: String,body:String) {
-        let content = UNMutableNotificationContent()
-        content.title = title
-        content.subtitle = body
-        content.sound = UNNotificationSound.default
 
-        // show this notification five seconds from now
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-
-        // choose a random identifier
-        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-
-        // add our notification request
-        UNUserNotificationCenter.current().add(request)
-        
     }
-    
+
     @objc(getRequest:)
     func getRequest( command: CDVInvokedUrlCommand) {
+        var pluginResult = CDVPluginResult(status: CDVCommandStatus_OK,messageAs:"Nie podano URL!")
+        //let param = command.argument(at: 0) as! String?
+        let param = command.arguments[0] as? String
+       // urll = command.arguments[0]
+        if param == nil {
+            print("NILLLLLLL")
+            pluginResult?.keepCallback = false
+            self.commandDelegate.send(pluginResult, callbackId:command.callbackId)
+            return
+        }
+        let parame = param
+        if parame?.isEmpty == true {
+            pluginResult = CDVPluginResult(status: CDVCommandStatus_OK,messageAs:"Zbyt krótki URL!")
+            pluginResult?.keepCallback = false
+            self.commandDelegate.send(pluginResult, callbackId:command.callbackId)
+            return
+        }
+        print("SmartRequestPlugin :: test is called with \(param)")
 
-        print("SmartRequestPlugin :: test is called")
-        
-        
-            
-        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK)
-        commandDelegate.send(pluginResult, callbackId:command.callbackId)
+        let url = URL(string: parame!)
+        guard let requestUrl = url else { fatalError() }
+        // Create URL Request
+        var request = URLRequest(url: requestUrl)
+        // Specify HTTP Method to use
+        request.httpMethod = "GET"
+        // Send HTTP Request
+
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+
+            // Check if Error took place
+            if let error = error {
+                print("Error took place \(error)")
+                pluginResult = CDVPluginResult(status: CDVCommandStatus_OK,messageAs:"Error took place \(error)")
+                pluginResult?.keepCallback = true
+                self.commandDelegate.send(pluginResult, callbackId:command.callbackId)
+                return
+            }
+
+            // Read HTTP Response Status code
+            if let response = response as? HTTPURLResponse {
+                print("Response HTTP Status code: \(response.statusCode)")
+                pluginResult = CDVPluginResult(status: CDVCommandStatus_OK,messageAs:"Response HTTP Status code: \(response.statusCode)")
+                pluginResult?.keepCallback = true
+                self.commandDelegate.send(pluginResult, callbackId:command.callbackId)
+            }
+
+            // Convert HTTP Response Data to a simple String
+            if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                print("Response data string:\n \(dataString)")
+                pluginResult = CDVPluginResult(status: CDVCommandStatus_OK,messageAs:"Response data string:\n \(dataString)")
+                pluginResult?.keepCallback = true
+                self.commandDelegate.send(pluginResult, callbackId:command.callbackId)
+            }
+
+
+
+        }
+        task.resume()
+
+
     }
     @objc(ready:)
     func ready( command: CDVInvokedUrlCommand) {
